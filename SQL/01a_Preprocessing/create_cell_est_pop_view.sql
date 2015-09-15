f59899fe8cd1f055b397a84900b07a2d031a058c
Nils Breyer <nbnet@arcor.de>
2015-09-15 14:21:50 +0200

    naming

--- SQL/01a_Preprocessing/create_cell_est_pop_view.sql
+++ SQL/01a_Preprocessing/create_cell_est_pop_view.sql
@@ -1,6 +1,6 @@
 DROP MATERIALIZED VIEW IF EXISTS cell_est_pop;
 
 CREATE MATERIALIZED VIEW cell_est_pop AS (
-SELECT antenna_id, SUM(hits) FROM ehomebase
+SELECT antenna_id AS cell_id, SUM(hits) AS population FROM ehomebase
 GROUP BY antenna_id
 );
